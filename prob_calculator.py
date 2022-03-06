f4d8d55ed604dc2c712349405da82d6b9335a9cb
Jordan Wendell <37525787+Jfoobar@users.noreply.github.com>
2022-03-06 20:59:37 +0000

    Fixed logic error at M+=1

--- prob_calculator.py
+++ prob_calculator.py
@@ -6,17 +6,17 @@ from collections import Counter
 class Hat:
   def __init__(self,**kwargs):
     self.contents = [k for k,v in kwargs.items() for i in range(v)]
-    print(self.contents)
+    self.cpy_contents = self.contents[:]  
     
   def draw(self, num):
     if num>len(self.contents):
-      return self.contents
+      #print(self.contents)
+      return self.cpy_contents
     arr =[]
     for i in range(num):
       rand_choice = random.choice(self.contents)
       arr.append(rand_choice)
-      if rand_choice in self.contents:
-        self.contents.remove(rand_choice)
+      self.contents.remove(rand_choice)
     return arr
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
@@ -28,10 +28,14 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     expected = True
     for color in expected_balls:
       try:
-        if expected_balls[color] != draw_count[color]:
+        if expected_balls[color] > draw_count[color]:
+          #print(draw_count)
           expected = False
       except:
+        #print(draw_count)
         expected = False
-      if expected:
-        M +=1
+        
+    if expected:
+      M +=1
+      #print(draw_count, draw_count[color])
   return M/num_experiments
